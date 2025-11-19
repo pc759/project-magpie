13a22649a6043196e69a9e3667951f74162ea9ab
pc759 <53225839+pc759@users.noreply.github.com>
2025-11-19 12:30:38 +0000

    Update MainActivity to display MainMenuScreen

--- app/src/main/java/com/spotitworld/MainActivity.kt
+++ app/src/main/java/com/spotitworld/MainActivity.kt
@@ -3,26 +3,39 @@ package com.spotitworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.spotitworld.ui.screens.MainMenuScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Spot It World")
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier,
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainMenuScreen(
+                        onStartNewHunt = {
+                            // TODO: Navigate to SetupScreen
+                        }
+                    )
+                }
+            }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Greeting("Android")
+fun MainMenuScreenPreview() {
+    MaterialTheme {
+        MainMenuScreen(
+            onStartNewHunt = {}
+        )
+    }
 }
