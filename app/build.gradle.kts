6cd6f92bd2a2666714f96d5f110362432ae8c583
pc759 <53225839+pc759@users.noreply.github.com>
2025-11-22 11:57:42 +0000

    Replace kotlin.compose alias with direct plugin ID"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24"
     kotlin("kapt") version "1.9.24"
 }
 
